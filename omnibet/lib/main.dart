d5a0aba055548f3ff6c46ce3a63abb8b2eed399a
tristan <tdacier@sicalde.fr>
2024-05-21 12:34:42 +0200

    pseudo affichage

--- omnibet/lib/main.dart
+++ omnibet/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'tournamentPage.dart';  // Assurez-vous d'importer la page des tournois
 import 'login_page.dart';
 import 'register_page.dart';
@@ -37,23 +41,75 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Home Page Content'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
+            FutureBuilder<String>(
+              future: _getUserInfo(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text('Bienvenue, ${snapshot.data}');
+                } else if (snapshot.hasError) {
+                  return Text('Erreur : ${snapshot.error}');
+                }
+                return CircularProgressIndicator();
               },
-              child: Text('Go to Register'),
             ),
             ElevatedButton(
               onPressed: () {
-                Navigator.pushNamed(context, '/login');
+                Navigator.pushNamed(context, '/tournament');
               },
-              child: Text('Go to Login'),
+              child: Text('Voir les tournois'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _logout(context);
+              },
+              child: Text('Se déconnecter'),
             ),
           ],
         ),
       ),
     );
   }
+
+Future<String> _getUserInfo() async {
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('token');
+
+  if (token != null) {
+    // Récupérer l'ID de l'utilisateur à partir du token
+    final jwtPayload = json.decode(
+        ascii.decode(base64.decode(base64.normalize(token.split(".")[1]))));
+    final userId = jwtPayload['id'];
+
+    // Récupérer les informations de l'utilisateur à partir de la base de données
+    final url = Uri.parse('http://localhost:8080/mobileuser/getUserInfo/$userId');
+    final response = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+     
+    );
+
+    if (response.statusCode == 200) {
+      final userInfo = json.decode(response.body);
+      print(userInfo);
+      final userName = userInfo[0];
+      print(userName);
+      return 'Nom d\'utilisateur: $userName'; // Retourner le nom d'utilisateur
+    } else {
+     
+      return 'Erreur de récupération des informations de l\'utilisateur';
+    }
+  } else {
+    return 'Aucun utilisateur connecté';
+  }
 }
 
+
+  void _logout(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove('user_id'); // Supprimez l'ID de l'utilisateur des préférences partagées
+    Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false); // Redirigez vers la page de connexion et supprimez toutes les routes empilées
+  }
+}
