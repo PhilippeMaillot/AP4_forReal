3d25aecef7feac5533850be91b8e180b12bec09c
tristan <tdacier@sicalde.fr>
2024-05-21 14:43:14 +0200

    psuedo

--- omnibet/lib/main.dart
+++ omnibet/lib/main.dart
@@ -3,10 +3,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'tournamentPage.dart';  // Assurez-vous d'importer la page des tournois
+import 'tournamentPage.dart'; // Assurez-vous d'importer la page des tournois
 import 'login_page.dart';
 import 'register_page.dart';
-import 'viewBetPage.dart';  // Importez la page de visualisation des paris
+import 'viewBetPage.dart'; // Importez la page de visualisation des paris
 
 void main() {
   runApp(MyApp());
@@ -25,8 +25,12 @@ class MyApp extends StatelessWidget {
         '/register': (context) => RegisterPage(),
         '/login': (context) => LoginPage(),
         '/home': (context) => MyHomePage(),
-        '/tournament': (context) => TournamentListPage(),  // Ajoutez la route '/tournament
-        '/viewBetPage': (context) => ViewBetPage(userId: 1,),  // Ajoutez la route '/viewBetPage
+        '/tournament': (context) =>
+            TournamentListPage(), // Ajoutez la route '/tournament
+        '/viewBetPage': (context) =>
+            ViewBetPage(
+              userId: 1,
+            ), // Ajoutez la route '/viewBetPage
       },
     );
   }
@@ -39,11 +43,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  late Future<String> _userInfoFuture; // Ajout de la variable Future
+
+  @override
+  void initState() {
+    super.initState();
+    _userInfoFuture = _getUserInfo(); // Appel à _getUserInfo() pour récupérer les infos utilisateur au démarrage
+  }
 
   static List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page Content'),  // Contenu de la page d'accueil
-    TournamentListPage(),       // Page des tournois
-    ViewBetPage(userId: 1),     // Page des paris
+    Text('Home Page Content'), // Contenu de la page d'accueil
+    TournamentListPage(), // Page des tournois
+    ViewBetPage(
+        userId:
+            1), // Page des paris
   ];
 
   void _onItemTapped(int index) {
@@ -56,7 +69,27 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: FutureBuilder<String>(
+          future: _userInfoFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Container(); // Retourne un widget vide pendant que les infos sont chargées
+            } else {
+              if (snapshot.hasError) {
+                return Text(
+                    'Erreur'); // Affiche un message d'erreur s'il y a eu une erreur
+              } else {
+                return Text(
+                  snapshot.data ?? '',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ); // Affiche le nom d'utilisateur une fois qu'il est disponible
+              }
+            }
+          },
+        ),
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
@@ -83,47 +116,47 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-Future<String> _getUserInfo() async {
-  final prefs = await SharedPreferences.getInstance();
-  final token = prefs.getString('token');
+  Future<String> _getUserInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
 
-  if (token != null) {
-    // Récupérer l'ID de l'utilisateur à partir du token
-    final jwtPayload = json.decode(
-        ascii.decode(base64.decode(base64.normalize(token.split(".")[1]))));
-    final userId = jwtPayload['id'];
+    if (token != null) {
+      // Récupérer l'ID de l'utilisateur à partir du token
+      final jwtPayload = json.decode(ascii.decode(base64.decode(
+          base64.normalize(token.split(".")[1]))));
+      final userId = jwtPayload['id'];
 
-    // Récupérer les informations de l'utilisateur à partir de la base de données
-    final url = Uri.parse('http://localhost:8080/mobileuser/getUserInfo/$userId');
-    final response = await http.get(
-      url,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-     
-    );
+      // Récupérer les informations de l'utilisateur à partir de la base de données
+      final url = Uri.parse(
+          'http://localhost:8080/mobileuser/getUserInfo/$userId');
+      final response = await http.get(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
 
-    if (response.statusCode == 200) {
-      final userInfo = json.decode(response.body);
-      print(userInfo);
-      final userName = userInfo[0];
-      print(userName);
-      return 'Nom d\'utilisateur: $userName'; // Retourner le nom d'utilisateur
+      if (response.statusCode == 200) {
+        final userInfo = json.decode(response.body);
+        print(userInfo);
+        final userName = userInfo[0]['user_name']; // Obtenez uniquement le nom d'utilisateur
+        print(userName);
+        return userName; // Retourner le nom d'utilisateur
+      } else {
+        return 'Erreur de récupération des informations de l\'utilisateur';
+      }
     } else {
-     
-      return 'Erreur de récupération des informations de l\'utilisateur';
+      return 'Aucun utilisateur connecté';
     }
-  } else {
-    return 'Aucun utilisateur connecté';
+  }
+
+  void _logout(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token'); // Supprimez le token des préférences partagées
+    await prefs.remove(
+        'user_id'); // Supprimez l'ID de l'utilisateur des préférences partagées
+    Navigator.pushNamedAndRemoveUntil(
+        context, '/login', (route) => false); // Redirigez vers la page de connexion et supprimez toutes les routes empilées
   }
 }
-void _logout(BuildContext context) async {
-  final prefs = await SharedPreferences.getInstance();
-  await prefs.remove('token'); // Supprimez le token des préférences partagées
-  await prefs.remove('user_id'); // Supprimez l'ID de l'utilisateur des préférences partagées
-  Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false); // Redirigez vers la page de connexion et supprimez toutes les routes empilées
-}
-
-} 
-
